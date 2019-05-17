d2e9369da657646a709c307472c19845955c390d
ajrominger <ajrominger@gmail.com>
2019-05-16 23:12:21 -0600

    got power analysis function working

--- compostAdd_power.R
+++ compostAdd_power.R
@@ -1,19 +1,31 @@
-caPower <- function(ntreat, nrep, blocked, mainEffects, blockEffectSD, errorSD) {
-    # browser()
+caPower <- function(ntreat, nrep, blocked, mainEffect, blockEffectSD, errorSD) {
     dat <- data.frame(rep = rep(1:nrep, each = ntreat), t = paste0('t', 0:(ntreat - 1)))
     
-    if(blocked) {
-        dat$b <- paste0('b', dat$rep)
-        mm <- model.matrix(~ 0 + b + t, dat)
-        p <- c(rnorm(nrep, 0, blockEffectSD), mainEffects[-1])
-    } else {
-        mm <- model.matrix(~ t)
-        p <- mainEffects
-    }
+    mainEffects <- 0:(ntreat - 1) * mainEffect
+    
+    
+    dat$b <- if(blocked) {dat$rep} else {1:nrow(dat)}
+    dat$b <- paste0('b', dat$b)
+    # browser()
+    
+    mm <- model.matrix(~ 0 + b + t, dat)
+    p <- c(rnorm(length(unique(dat$b)), 0, blockEffectSD), mainEffects[-1])
     
     dat$y <- mm %*% p + rnorm(nrow(dat), 0, errorSD)
     
-    mod <- lme4::lmer(y ~ t + (1 | b), dat)
+    if(blocked) {
+        mod1 <- logLik(lme4::lmer(y ~ t + (1 | b), dat))
+        mod0 <- logLik(lme4::lmer(y ~ 1 | b, dat))
+    } else {
+        mod1 <- logLik(lm(y ~ t, dat))
+        mod0 <- logLik(lm(y ~ 1, dat))
+    }
+    
+    pval <- pchisq(-2 * (mod0 - mod1), df = attr(mod1, 'df') - attr(mod0, 'df'), 
+                   lower.tail = FALSE)
+    
+    return(as.numeric(pval))
 }
 
-caPower(4, 6)
+caPower(5, 6, blocked = FALSE, mainEffect = 0.5, blockEffectSD = 0.5, errorSD = 0.5)
+
